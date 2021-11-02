ad0067e43826f910be893c0ea9178ce483722b36
Jaclyn Ippolito <91428902+Jaclyn-Ippolito@users.noreply.github.com>
2021-11-02 17:41:38 -0500

    add JS code for mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hey There! Welcome to My Portfolio Site")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
