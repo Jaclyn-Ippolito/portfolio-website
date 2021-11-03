cd1e16f1d0860ee8a9e1fd00f4ab232eb0abc49a
Jaclyn Ippolito <91428902+Jaclyn-Ippolito@users.noreply.github.com>
2021-11-02 20:53:32 -0500

    add smooth scroll to contact

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,20 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
