68d3b5207c6759834afcd440a0b565f7c6b0b39d
Jaclyn Ippolito <91428902+Jaclyn-Ippolito@users.noreply.github.com>
2021-11-06 12:55:31 -0500

    Add JS for pg tab underline

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 console.log("Hey There! Welcome to My Portfolio Site")
 
+let currentPage = window.location.href;
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
@@ -7,9 +9,28 @@ function menuToggle() {
   } else {
     x.className = 'navtoggle';
   }
+  getPage();
+}
+
+function getPage() {
+  currentPage = window.location.href;
+  $('#work-tab').removeClass("underline-tab")
+  $('#about-tab').removeClass("underline-tab")
+  $('#contact-tab').addClass("underline-tab")
 }
 
 $(document).ready(function() {
+  let pageArray = currentPage.split("/");
+  let pageArrayLength = pageArray.length;
+  let targetPage = pageArray[pageArrayLength - 1];
+
+  if (targetPage === "index.html") {
+    $('#work-tab').addClass("underline-tab")
+  }
+  if (targetPage === "about.html") {
+    $('#about-tab').addClass("underline-tab")
+  }
+
   $("a").on('click', function(event) {
 
     if (this.hash !== "") {
